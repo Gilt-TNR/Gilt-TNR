e3a5ce619273bc95f7d84713f9e05b8385ff0bfe
Markus Hauru <markus@mhauru.org>
2017-09-18 12:45:24 -0400

    Add proper GiltTNR2D_test.py, also _setup.py

---
+++ GiltTNR2D_setup.py
@@ -0,0 +1,91 @@
+import GiltTNR2D
+import logging
+
+version = GiltTNR2D.version
+
+parinfo = {
+    "iter_count": {
+        "default": 0,
+        "idfunc":  lambda dataname, pars: True
+    },
+
+    # Parameters for disentangling.
+    "gilt_eps": {
+        "default": 1e-6,
+        "idfunc":  lambda dataname, pars: not bool(pars["gilt_eps_list"])
+    },
+    "gilt_eps_list": {
+        "default": None,
+        "idfunc":  lambda dataname, pars: bool(pars["gilt_eps_list"])
+    },
+    "gilt_print_corspec": {
+        "default": False,
+        "idfunc":  lambda dataname, pars: False
+    },
+
+    # Parameters for the coarse-graining.
+    "cg_chis": {
+        "default": [1,2,3,4,5,6],
+        "idfunc":  lambda dataname, pars: True
+    },
+    "cg_eps": {
+        "default": 1e-3,
+        "idfunc":  lambda dataname, pars: True
+    },
+
+    # Other parameters
+    "verbosity": {
+        "default": 10,
+        "idfunc":  lambda dataname, pars: False
+    },
+}
+
+
+def generate(dataname, *args, pars=dict(), filelogger=None):
+    infostr = ("{}"
+               "\nGenerating {} with Gilt-TNR2D (version {})."
+               "\niter_count = {}"
+               .format("="*70, dataname, version, pars["iter_count"]))
+    logging.info(infostr)
+    if filelogger is not None:
+        # Only print the dictionary into the log file, not in stdout.
+        dictstr = ""
+        for k,v in sorted(pars.items()):
+           dictstr += "\n%s = %s"%(k, v)
+        filelogger.info(dictstr)
+
+    iter_count = pars["iter_count"]
+    if pars["gilt_eps_list"]:
+        pars = pars.copy()
+        try:
+            pars["gilt_eps"] = pars["gilt_eps_list"][iter_count-1]
+        except IndexError:
+            pars["gilt_eps"] = pars["gilt_eps_list"][-1]
+
+    if dataname == "A":
+        res = generate_A(*args, pars=pars)
+    else:
+        raise ValueError("Unknown dataname: {}".format(dataname))
+    return res
+
+
+def generate_A(*args, pars=dict()):
+    A, log_fact = args[0]
+    res = GiltTNR2D.gilttnr_step(A, log_fact, pars)
+    return res
+
+
+def prereq_pairs(dataname, pars):
+    if dataname == "A":
+        res = prereq_pairs_A(pars)
+    else:
+        raise ValueError("Unknown dataname: {}".format(dataname))
+    return res
+
+
+def prereq_pairs_A(pars):
+    prereq_pars = pars.copy()
+    prereq_pars["iter_count"] -= 1
+    res = [("A", prereq_pars)]
+    return res
+
--- GiltTNR2D_test.py
+++ GiltTNR2D_test.py
@@ -1,56 +1,64 @@
 import numpy as np
 import sys
 import os
+import warnings
 import logging
 import logging.config
 import configparser
 import datetime
-import multilineformatter
-import scipy.integrate as integrate
-from GiltTNR2D import gilttnr_step
-from tensors import Tensor, TensorZ2
+from tntools import modeldata, datadispenser, multilineformatter
+from tntools.yaml_config_parser import parse_argv
+from tntools.ncon_sparseeig import ncon_sparseeig
 from ncon import ncon
-from yaml_config_parser import parse_argv
 
 tools_path = os.path.dirname(multilineformatter.__file__)
 logging.config.fileConfig(tools_path + "/logging_default.conf")
 filename = os.path.basename(__file__).replace(".py", "")
 np.set_printoptions(precision=10)
 
-default_pars = {
-    # Parameters for this test file.
-    "beta": np.log(1 + np.sqrt(2)) / 2,
-    "total_iters": 6,
-    "symmetry_tensors": True,
-    "print_scaldims": 50,
-    "print_spectra": True,
-    "print_freeenergy": True,
-    "log_dir": "logs",
 
-    # Parameters for Gilt-TNR.
-    "gilt_eps": 1e-6,
-    "cg_chis": [1,2,3,4,5,6],
-    "cg_eps": 1e-10,
-    "verbosity": 100
+parinfo = {
+    "iters": {
+        "default": range(1, 6),
+    },
+    "print_scaldims": {
+        "default": 50,
+    },
+    "print_spectra": {
+        "default": True,
+    },
+    "print_free_energy": {
+        "default": True,
+    },
+    "debug": {
+        "default": False
+    },
+    "database": {
+        "default": "data/GiltTNR2D/"
+    }
 }
 
 
 def parse():
     pars = parse_argv(sys.argv)
+    pars["algorithm"] = "GiltTNR2D"
     return pars
 
 
-def apply_default_pars(pars, default_pars):
-    for k, v in default_pars.items():
+def apply_default_pars(pars, parinfo):
+    for k, v in parinfo.items():
         if k not in pars:
-            pars[k] = v
+            pars[k] = v["default"]
     return
 
 
 def set_filehandler(logger, logfilename, pars):
     os.makedirs(os.path.dirname(logfilename), exist_ok=True)
     filehandler = logging.FileHandler(logfilename, mode='w')
-    filehandler.setLevel(logging.INFO)
+    if pars["debug"]:
+        filehandler.setLevel(logging.DEBUG)
+    else:
+        filehandler.setLevel(logging.INFO)
     parser = configparser.ConfigParser(interpolation=None)
     parser.read(tools_path + '/logging_default.conf')
     fmt = parser.get('formatter_default', 'format')
@@ -66,20 +74,6 @@ def get_free_energy(A, log_fact, pars, iter_count):
     log_Z = np.log(Z) + log_fact
     F = -log_Z/pars["beta"]
     f = F/(2*4**(iter_count))
-    return f
-
-
-def get_exact_free_energy(pars):
-    beta = pars["beta"]
-    sinh = np.sinh(2*beta)
-    cosh = np.cosh(2*beta)
-    def integrand(theta):
-        res = np.log(cosh**2 +
-                     sinh**2 * np.sqrt(1+sinh**(-4) -
-                                       2*sinh**(-2)*np.cos(theta)))
-        return res
-    integral, err = integrate.quad(integrand, 0, np.pi)
-    f = -(np.log(2)/2 + integral/(2*np.pi)) / beta
     return f
 
 
@@ -99,6 +93,7 @@ def get_scaldims(A, pars):
     # Extract the scaling dimensions from the eigenvalues of the
     # transfer matrix.
     es = es.to_ndarray()
+    es = np.abs(es)
     es = -np.sort(-es)
     es[es==0] += 1e-16  # Ugly workaround for taking the log of zero.
     log_es = np.log(es)
@@ -107,34 +102,20 @@ def get_scaldims(A, pars):
     return log_es
 
 
-def get_initial_tensor(pars):
-    hamiltonian = np.array([[-1, 1], [ 1,-1]])
-    boltz = np.exp(-pars["beta"]*hamiltonian)
-    A_0 = np.einsum('ab,bc,cd,da->abcd', boltz, boltz, boltz, boltz)
-    u = np.array([[1, 1], [1,-1]]) / np.sqrt(2)
-    u_dg = u.T.conjugate()
-    A_0 = ncon((A_0, u, u, u_dg, u_dg),
-               ([1,2,3,4], [-1,1], [-2,2], [3,-3], [4,-4]))
-    if pars["symmetry_tensors"]:
-        dim, qim = [1,1], [0,1]
-        A_0 = TensorZ2.from_ndarray(A_0, shape=[dim]*4, qhape=[qim]*4,
-                                    dirs=[1,1,-1,-1])
-    else:
-        A_0 = Tensor.from_ndarray(A_0)
-    return A_0
-
-
 if __name__ == "__main__":
     pars = parse()
-    apply_default_pars(pars, default_pars)
+    apply_default_pars(pars, parinfo)
+    datadispenser.update_default_pars("A", pars, iter_count=max(pars["iters"]))
+
+    if pars["debug"]:
+        warnings.filterwarnings('error')
 
     datetime_str = datetime.datetime.strftime(datetime.datetime.now(),
                                              '%Y-%m-%d_%H-%M-%S')
     title_str = ('{}_{}_beta{}_cgchis{}_deeps{}'
                  .format(filename, datetime_str, pars["beta"],
                          pars["cg_chis"][-1], pars["gilt_eps"]))
-
-    logfilename = "{}/{}.log".format(pars["log_dir"], title_str)
+    logfilename = "logs/{}.log".format(title_str)
     rootlogger = logging.getLogger()
     set_filehandler(rootlogger, logfilename, pars)
 
@@ -145,21 +126,23 @@ if __name__ == "__main__":
         infostr += "\n%s = %s"%(k, v)
     logging.info(infostr)
 
-    A = get_initial_tensor(pars)
-    log_fact = 0
+    dbname = pars["database"]
 
-    for iter_count in range(1, pars["total_iters"]+1):
-        logging.info("\nIteration {}".format(iter_count))
-        A, log_fact = gilttnr_step(A, log_fact, pars)
+    for it in pars["iters"]:
+        logging.info("\nIteration {}".format(it))
+        res = datadispenser.get_data(
+            dbname, "A", pars, iter_count=it
+        )
+        A, log_fact = res[0], res[1]
 
         if pars["print_spectra"]:
             es = get_A_spectrum(A)
             msg = "Spectrum of A:\n{}".format(es[:30])
             logging.info(msg)
 
-        if pars["print_freeenergy"]:
-            f = get_free_energy(A, log_fact, pars, iter_count)
-            exact_f = get_exact_free_energy(pars)
+        if pars["print_free_energy"]:
+            f = get_free_energy(A, log_fact, pars, it)
+            exact_f = modeldata.get_free_energy(pars)
             f_error = np.abs(f - exact_f)/exact_f
             msg = ("Free energy per site: {} ({}, off by {:.4e})"
                    .format(f, exact_f, f_error))
